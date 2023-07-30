17d4fff5f41860be388fbc14e72ccfd83fc56d5e
Lawrence-tech <lawrencesagini2@gmail.com>
2023-07-30 14:57:23 +0300

    pycodestye fix

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,6 +3,7 @@
     Returns a list of lists of integers representing the Pascal’s triangle of n
 """
 
+
 def pascal_triangle(n):
     """
     return a list of lists of integers representing the Pascal’s triangle of n
