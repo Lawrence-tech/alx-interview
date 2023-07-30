14a959392c6bc85b45370c5a320e32cbfbf4fff3
Lawrence-tech <lawrencesagini2@gmail.com>
2023-07-30 14:47:29 +0300

    0. Pascal's Triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,19 @@
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    # Initialize the result list with the first row containing 1
+    result = [[1]]
+
+    for i in range(1, n):
+        # Calculate the next row based on the previous row
+        prev_row = result[-1]
+        current_row = [1]  # First element is always 1
+        for j in range(1, i):
+            current_row.append(prev_row[j - 1] + prev_row[j])
+        current_row.append(1)  # Last element is always 1
+
+        # Add the current row to the result list
+        result.append(current_row)
+
+    return result
