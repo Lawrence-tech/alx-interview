af10c38cd0ccde1b7d93d4a525b38573110d2cb1
Lawrence-tech <lawrencesagini2@gmail.com>
2023-08-10 12:23:56 +0300

    Minimum Operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+"""
+Minimum operation
+"""
+
+
+def minOperations(n):
+    if n <= 1:
+        return 0
+
+    operations = 0
+    divisor = 2
+
+    while n > 1:
+        while n % divisor == 0:
+            operations += divisor
+            n //= divisor
+        divisor += 1
+
+    return operations
+
+
+# Test cases
+n = 4
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 12
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
