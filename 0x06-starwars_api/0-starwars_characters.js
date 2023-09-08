11db711bfdfc97b1610d6c965df75a2fadae7c6f
Lawrence-tech <lawrencesagini2@gmail.com>
2023-09-08 03:24:39 +0300

    Modified Star Wars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,25 +1,40 @@
 #!/usr/bin/node
 const request = require('request');
-const API_URL = 'https://swapi-api.hbtn.io/api';
+const API_URL = 'https://swapi.dev/api';
 
 if (process.argv.length > 2) {
-  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
+  request(`${API_URL}/films/${process.argv[2]}/`, (err, response, body) => {
     if (err) {
-      console.log(err);
+      console.error(err);
+      return;
     }
-    const charactersURL = JSON.parse(body).characters;
-    const charactersName = charactersURL.map(
-      url => new Promise((resolve, reject) => {
-        request(url, (promiseErr, __, charactersReqBody) => {
-          if (promiseErr) {
-            reject(promiseErr);
-          }
-          resolve(JSON.parse(charactersReqBody).name);
-        });
-      }));
 
-    Promise.all(charactersName)
-      .then(names => console.log(names.join('\n')))
-      .catch(allErr => console.log(allErr));
+    if (response.statusCode !== 200) {
+      console.error(`Request failed with status code ${response.statusCode}`);
+      return;
+    }
+
+    const charactersURLs = JSON.parse(body).characters;
+    const characterPromises = charactersURLs.map(characterURL => {
+      return new Promise((resolve, reject) => {
+        request(characterURL, (characterErr, _, characterBody) => {
+          if (characterErr) {
+            reject(characterErr);
+          } else {
+            resolve(JSON.parse(characterBody).name);
+          }
+        });
+      });
+    });
+
+    Promise.all(characterPromises)
+      .then(characterNames => {
+        console.log(characterNames.join('\n'));
+      })
+      .catch(allErr => {
+        console.error(allErr);
+      });
   });
+} else {
+  console.log('Please provide a Movie ID as a command-line argument.');
 }
