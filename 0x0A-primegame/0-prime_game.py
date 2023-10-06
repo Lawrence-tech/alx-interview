43576ed25938f9964e676ad130e26f80c3417d90
Lawrence-tech <lawrencesagini2@gmail.com>
2023-10-06 04:03:03 +0300

    meet requirement

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,34 +1,60 @@
 #!/usr/bin/python3
 """
-Prime Game Interview Module
+Prime Interview Game Challenge Module
 """
-from round_winner import round_winner
 
 
-def isWinner(x, num):
+def isWinner(x, nums):
     """
-    Function to determine the winner of the game,
-    after x number of rounds
+    Function to determine the winner of a game after x rounds
     """
-    ben, maria = 0, 0
+    def sieve_of_eratosthenes(n):
+        """
+        Use to find prime numbers and calculates the winner based on the count
+        """
+        is_prime = [True] * (n + 1)
+        is_prime[0] = is_prime[1] = False
+        primes = []
 
-    for i in range(x):
-        # The winner of each round gets 1 (a single) point
-        winner = round_winner(num[i])
+        for p in range(2, n + 1):
+            if is_prime[p]:
+                primes.append(p)
+                for i in range(p * p, n + 1, p):
+                    is_prime[i] = False
+
+        return primes
+
+    def calculate_winner(n):
+        """
+        Determine the winner of a single round
+        """
+        primes = sieve_of_eratosthenes(n)
+        if len(primes) % 2 == 0:
+            return "Ben"
+        else:
+            return "Maria"
+
+    maria_wins = 0
+    ben_wins = 0
+
+    for n in nums:
+        if n <= 1:
+            continue
+        winner = calculate_winner(n)
         if winner == "Ben":
-            ben += 1
-        elif winner == "Maria":
-            maria += 1
+            ben_wins += 1
+        else:
+            maria_wins += 1
 
-    # The player with the highest points after a full game wins
-    # The both playes get same or no points, there is no winner (None)
-    if ben > maria:
-        return "Ben"
-    elif maria > ben:
+    if maria_wins > ben_wins:
         return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
     else:
         return None
 
 
-if __name__ == "__main__":
-    print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 3])))
+# Test your function with example input
+x = 3
+nums = [4, 5, 1]
+print("Winner: {}".format(isWinner(x, nums)))  # Output should be "Ben"
